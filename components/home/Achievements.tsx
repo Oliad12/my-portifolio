7820b59113b80eae6c317b153c7441c96f9fbecf
Oliad Dere <tibebudere2023@gmail.com>
2026-03-08 17:48:25 +0300

    update achievements

--- components/home/Achievements.tsx
+++ components/home/Achievements.tsx
@@ -4,17 +4,17 @@ import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 import { achievementsList } from "@/assets/assets";
 
-
 const AnimatedNumbers = dynamic(() => import("react-animated-numbers"), {
   ssr: false,
 });
 
 export default function Achievements() {
- 
   return (
-    <section className="w-full mt-5 px-3 md:px-7 lg:px-24 py-5 scroll-mt-20"> 
-      <div className="sm:border rounded-md py-8 px-8 flex flex-col md:flex-row 
-      items-center justify-between gap-6 max-w-5xl mx-auto">
+    <section className="w-full px-6 lg:px-[12%] py-5 scroll-mt-20">
+      <div
+        className="max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6
+    py-8 px-8 rounded-xl md:border-y border-border"
+      >
         {achievementsList.map((achievement, index) => (
           <motion.div
             key={index}
@@ -23,26 +23,28 @@ export default function Achievements() {
             viewport={{ once: true }}
             transition={{ duration: 0.8, delay: 0.2 * index }}
             className="relative flex flex-col items-center justify-center p-6 w-40 md:w-48 rounded-xl 
-            bg-background/90 hover:scale-105 hover:shadow-lg transition-transform duration-300">
-            <div className="absolute top-0 left-0 w-full h-1 rounded-t-xl bg-linear-to-r
-             from-indigo-500 via-purple-500 to-pink-500 animate-[gradient-btlr_3s_linear_infinite]" />
-
-            <div className="absolute bottom-0 left-0 w-full h-1 rounded-b-xl bg-linear-to-r
-             from-pink-500 to-indigo-500 via-purple-500 animate-[gradient-tbrl_3s_linear_infinite]" />
-
+            bg-background/90 hover:scale-105 hover:shadow-lg transition-transform duration-300"
+          >
+            <div className="absolute top-0 left-0 w-full h-0.75 rounded-t-xl 
+              bg-linear-to-r from-red-500 via-white to-black 
+             bg-size-[200%_100%] animate-[flagMove_4s_linear_infinite]"
+            />
+            <div className="absolute bottom-0 left-0 w-full h-0.75 rounded-b-xl 
+              bg-linear-to-r from-black via-white to-red-500 
+              bg-size-[200%_100%] animate-[flagMoveReverse_4s_linear_infinite]"
+            />
             <h3 className="text-3xl sm:text-4xl font-bold flex items-center justify-center gap-1 relative z-10">
               {achievement.prefix || ""}
               <AnimatedNumbers
                 animateToNumber={Number(
-                  achievement.value.toString().replace("%", "")
+                  achievement.value.toString().replace("%", ""),
                 )}
                 fontStyle={{ fontWeight: 700 }}
               />
               {achievement.postfix || ""}
             </h3>
 
-            <p
-              className="mt-2 text-center text-sm font-Ovo relative z-10">
+            <p className="mt-2 text-center text-sm font-Ovo relative z-10">
               {achievement.metric || achievement.label}
             </p>
           </motion.div>
